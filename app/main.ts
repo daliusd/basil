1a2b487bf8d53146fd944ed2970c633da6417962
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2019-02-10 15:51:22 +0200

    Option to test with multiple different samples added.

--- app/main.ts
+++ app/main.ts
@@ -1,77 +1,29 @@
 import * as fs from 'fs';
-
+import * as prompt from 'prompt';
 import { JobData, generatePdf } from '../lib/pdfgen';
 
-const data: JobData = {
-    width: 63.5,
-    height: 88.9,
-    cardsAllIds: ['YKEWya2ef', 'e5wV1082R'],
-    cardsById: {
-        YKEWya2ef: {
-            id: 'YKEWya2ef',
-            count: 5,
-        },
-        e5wV1082R: {
-            id: 'e5wV1082R',
-            count: 5,
+const PATH_TO_SEARCH = '../icons/';
+const SERVER = 'http://localhost:5000';
+
+prompt.start();
+
+var schema = {
+    properties: {
+        sample: {
+            default: '01',
         },
     },
-    placeholders: {
-        o0Kh4zsgz: {
-            id: 'o0Kh4zsgz',
-            type: 'image',
-            x: 23.283333333333335,
-            y: 48.41875,
-            width: 37.041666666666664,
-            height: 35.71875,
-            angle: 0,
-        },
-        dpBZ7ZVEs: {
-            id: 'dpBZ7ZVEs',
-            type: 'text',
-            x: 2.910416666666667,
-            y: 6.879166666666666,
-            width: 56.62083333333334,
-            height: 13.229166666666666,
-            angle: -0.13017305440325466,
-            align: 'center',
-            color: '#000000',
-            fontFamily: 'Mountains of Christmas',
-            fontVariant: 'regular',
-            fontSize: 8,
-        },
-    },
-    texts: {
-        YKEWya2ef: {
-            dpBZ7ZVEs: {
-                value: 'RIVER',
-            },
-        },
-        e5wV1082R: {
-            dpBZ7ZVEs: {
-                value: 'MOUNTAIN',
-            },
-        },
-    },
-    images: {
-        YKEWya2ef: {
-            o0Kh4zsgz: {
-                url: '/api/imagefiles/delapouite river',
-            },
-        },
-        e5wV1082R: {
-            o0Kh4zsgz: {
-                url: '/api/imagefiles/delapouite mountain-cave',
-            },
-        },
-    },
-    pageWidth: 210,
-    pageHeight: 297,
-    topBottomMargin: 15,
-    leftRightMargin: 9,
 };
 
-const stream = fs.createWriteStream('output.pdf');
-generatePdf(data, 'http://localhost:5000', stream, () => {
-    console.log('finished');
+prompt.get(schema, function(err, result) {
+    generatePdfForSample(result.sample);
 });
+
+const generatePdfForSample = sampleFileName => {
+    const data: JobData = JSON.parse(fs.readFileSync(`sample${sampleFileName}.json`, 'utf8'));
+
+    const stream = fs.createWriteStream('output.pdf');
+    generatePdf(data, 'http://localhost:5000', stream, () => {
+        console.log('finished');
+    });
+};
