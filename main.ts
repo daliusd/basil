99e4edfab5c11ac306868a728c87f7def4390c9e
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2019-02-23 16:36:08 +0200

    Non working sample added.

--- main.ts
+++ main.ts
@@ -1,22 +1,10 @@
 import * as fs from 'fs';
-import * as prompt from 'prompt';
 import { JobData, generatePdf } from './lib/pdfgen';
 import * as yargs from 'yargs';
 
 const SERVER = 'https://cardamon.ffff.lt';
 
-prompt.override = yargs.argv;
-prompt.start();
-
-var schema = {
-    properties: {
-        sample: {
-            default: '01',
-        },
-    },
-};
-
-const generatePdfForSample = sampleFileName => {
+const generatePdfForSample = (sampleFileName: string) => {
     const data: JobData = JSON.parse(fs.readFileSync(`samples/sample${sampleFileName}.json`, 'utf8'));
 
     const stream = fs.createWriteStream('output.pdf');
@@ -25,6 +13,4 @@ const generatePdfForSample = sampleFileName => {
     });
 };
 
-prompt.get(schema, function(err, result) {
-    generatePdfForSample(result.sample);
-});
+generatePdfForSample(yargs.argv['sample'] as string);
