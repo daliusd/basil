ba8ae95cb8ba24830be26dba9142ebe66dd90ab8
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2019-03-18 18:55:53 +0200

    Rendering improvements.

--- lib/pdfgen.ts
+++ lib/pdfgen.ts
@@ -308,32 +308,39 @@ export const generatePdf = async (
                         doc.translate((-placeholder.width / 2) * PTPMM, (-placeholder.height / 2) * PTPMM);
 
                         if (imageInfo.base64) {
-                            SVGtoPDF(doc, atob(imageInfo.base64), 0, 0, {
+                            const svg = Buffer.from(imageInfo.base64, 'base64');
+                            SVGtoPDF(doc, svg.toString(), 0, 0, {
                                 width: placeholder.width * PTPMM,
                                 height: placeholder.height * PTPMM,
                             });
-                        } else {
-                            let resp = await makeRequest(serverUrl + imageInfo.url);
-                            const buf = buffer.Buffer.from(resp.data);
+                        } else if (imageInfo.url) {
+                            try {
+                                let resp = await makeRequest(serverUrl + imageInfo.url);
+                                const buf = buffer.Buffer.from(resp.data);
 
-                            if (resp.headers['content-type'] === 'image/svg+xml') {
-                                SVGtoPDF(doc, buf.toString(), 0, 0, {
-                                    width: placeholder.width * PTPMM,
-                                    height: placeholder.height * PTPMM,
-                                    preserveAspectRatio:
-                                        placeholder.fit === 'height' ? 'xMinYMid slice' : 'xMidYMin meet',
-                                });
-                            } else {
-                                doc.image(buf, 0, 0, {
-                                    width:
-                                        !placeholder.fit || placeholder.fit === 'width' || placeholder.fit === 'stretch'
-                                            ? placeholder.width * PTPMM
-                                            : undefined,
-                                    height:
-                                        placeholder.fit === 'height' || placeholder.fit === 'stretch'
-                                            ? placeholder.height * PTPMM
-                                            : undefined,
-                                });
+                                if (resp.headers['content-type'] === 'image/svg+xml') {
+                                    SVGtoPDF(doc, buf.toString(), 0, 0, {
+                                        width: placeholder.width * PTPMM,
+                                        height: placeholder.height * PTPMM,
+                                        preserveAspectRatio:
+                                            placeholder.fit === 'height' ? 'xMinYMid slice' : 'xMidYMin meet',
+                                    });
+                                } else {
+                                    doc.image(buf, 0, 0, {
+                                        width:
+                                            !placeholder.fit ||
+                                            placeholder.fit === 'width' ||
+                                            placeholder.fit === 'stretch'
+                                                ? placeholder.width * PTPMM
+                                                : undefined,
+                                        height:
+                                            placeholder.fit === 'height' || placeholder.fit === 'stretch'
+                                                ? placeholder.height * PTPMM
+                                                : undefined,
+                                    });
+                                }
+                            } catch {
+                                // TODO: handle error here
                             }
                         }
 
