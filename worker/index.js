29b4d31909a0736985c91e7b0fc9fcc58077d7d3
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2019-06-09 11:26:04 +0300

    Exorcist added.

--- worker/index.js
+++ worker/index.js
@@ -1,11 +1,13 @@
 var browserify = require('browserify');
 var tsify = require('tsify');
+var exorcist = require('exorcist');
 
-browserify()
+browserify({ debug: true })
     .add('worker.ts')
     .plugin(tsify, { noImplicitAny: true })
     .transform('uglifyify', { global: true })
     .bundle()
+    .pipe(exorcist('../../saffron/public/js/worker.js.map'))
     .on('error', function(error) {
         console.error(error.toString());
     })
